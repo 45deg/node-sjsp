91569533e540a0a1a85dd82be05f6d978d4eea15
azu <azuciao@gmail.com>
2017-11-14 12:08:11 +0900

    test(injector): add tests

---
+++ test/fixtures/if-else-no-block-return/output.js
@@ -0,0 +1,16 @@
+function fn() {
+    var sjsp__state = sjsp__start('example.js', 1, 15, 'fn', 'function fn() {');
+    if (true)
+        return function (arguments) {
+            var sjsp__return = 1;
+            sjsp__end(sjsp__state);
+            return sjsp__return;
+        }.call(this, arguments);
+    else
+        return function (arguments) {
+            var sjsp__return = 2;
+            sjsp__end(sjsp__state);
+            return sjsp__return;
+        }.call(this, arguments);
+    sjsp__end(sjsp__state);
+}
